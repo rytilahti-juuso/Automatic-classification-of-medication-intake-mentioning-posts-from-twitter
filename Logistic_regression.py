b1add94832566c398778fd08fbf1b91b74b6d965
rytilahti-juuso <rytilahti-juuso@users.noreply.github.com>
2019-03-27 12:37:39 +0200

    added formatted_data
    Contains only useful information

--- Logistic_regression.py
+++ Logistic_regression.py
@@ -1,7 +1,10 @@
 #import os
 #os.chdir(r'C:\Users\The Risk Chief\Documents\GitHub\Automatic-classification-of-medication-intake-mentioning-posts-from-twitter')
 import pandas as pd
+
+#Importing data
 column_names = ["tweet_id", "name", "train_id", "rank", "text"]
 data = pd.read_csv('TW_ST_2/train.txt', delimiter= "\t", names = column_names)
+formatted_data = data.loc[:, ["rank", "text"]]
 
 
